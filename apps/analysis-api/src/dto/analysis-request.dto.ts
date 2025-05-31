fc8b60ff0d61b8df02ef56bc481fb9b963f4711d
mkuranda <kuranda.marek@gmail.com>
2025-05-31 12:40:11 +0200

    dto

---
+++ apps/analysis-api/src/dto/analysis-request.dto.ts
@@ -0,0 +1,15 @@
+import { IsString, IsNotEmpty, MinLength, MaxLength } from 'class-validator';
+import { Transform } from 'class-transformer';
+
+export class AnalysisRequestDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(5, {
+        message: 'Message must be at least 5 characters long'
+    })
+    @MaxLength(1000, {
+        message: 'Message cannot be longer than 1000 characters'
+    })
+    @Transform(({ value }) => value.trim())
+        message: string;
+}
